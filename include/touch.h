774260c1e596ce3ba69d186da1494a582f70958a
Jordan Bursik <jsbursik@gmail.com>
2025-06-28 01:25:57 -0500

    first commit

---
+++ include/touch.h
@@ -0,0 +1,50 @@
+#define TOUCH_GT911
+#define TOUCH_GT911_SCL 20 // 20
+#define TOUCH_GT911_SDA 19 // 19
+#define TOUCH_GT911_INT -1 //-1
+#define TOUCH_GT911_RST -1 // 38
+#define TOUCH_GT911_ROTATION ROTATION_NORMAL
+#define TOUCH_MAP_X1 800 // 480
+#define TOUCH_MAP_X2 0
+#define TOUCH_MAP_Y1 480 // 272
+#define TOUCH_MAP_Y2 0
+
+int touch_last_x = 0, touch_last_y = 0;
+
+#if defined(TOUCH_GT911)
+#include <Wire.h>
+#include <TAMC_GT911.h>
+TAMC_GT911 ts = TAMC_GT911(TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT, TOUCH_GT911_RST, max(TOUCH_MAP_X1, TOUCH_MAP_X2), max(TOUCH_MAP_Y1, TOUCH_MAP_Y2));
+#endif
+
+void touch_init()
+{
+  Wire.begin(TOUCH_GT911_SDA, TOUCH_GT911_SCL);
+  ts.begin();
+  ts.setRotation(TOUCH_GT911_ROTATION);
+}
+
+bool touch_has_signal()
+{
+  return true;
+}
+
+bool touch_touched()
+{
+  ts.read();
+  if (ts.isTouched)
+  {
+    touch_last_x = map(ts.points[0].x, TOUCH_MAP_X1, TOUCH_MAP_X2, 0, lcd.width() - 1);
+    touch_last_y = map(ts.points[0].y, TOUCH_MAP_Y1, TOUCH_MAP_Y2, 0, lcd.height() - 1);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool touch_released()
+{
+  return true;
+}
