cf68df4aaad6f00222fa0861c37eda531de12a38
Jordan Bursik <jsbursik@gmail.com>
2025-07-01 10:27:48 -0500

    start adding global defines here

---
+++ include/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Define Backlight Pin
+#define TFT_BL 2
+
+// LCD Size
+#define LCD_WIDTH 800
+#define LCD_HEIGHT 480
+
+// LCD Buffer Size
+#define BUFFER_SIZE ((LCD_WIDTH * LCD_HEIGHT * 2) / 6)
+
+// Grid size
+#define GRID_COLS 8
+#define GRID_ROWS 4
